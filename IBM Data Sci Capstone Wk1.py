428de8d00b4e7414a6d74ba0c80ae7e78ac6a902
JaimePillar <60944577+JaimePillar@users.noreply.github.com>
2020-02-15 03:18:45 -0300

    Add files via upload

---
+++ IBM Data Sci Capstone Wk1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This Notebook is for Coursera's IBM Data Science Certification Program
+
+# Week 1 Assignment: "Hello, World"
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
